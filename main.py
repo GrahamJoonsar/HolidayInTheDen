32f9ee72ed1ddbd610704cfa3192973cff1f73bd
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-09-06 19:47:43 -0400

    Added sprites to snowmen

--- main.py
+++ main.py
@@ -9,18 +9,37 @@ win = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("SnowMan")
 numOfPlayers = 8
 bounciness = 3
+SnowManColors = [(255, 0, 0), 
+                (0, 255, 0),
+                (0, 0, 255), 
+                (255, 255, 0), 
+                (255, 0, 255), 
+                (0, 255, 255), 
+                (255, 100, 100), 
+                (165, 42, 42)]
+
 
 def distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 # Snowman(Player) class
-class Snowman:
-    def __init__(self, snowManNumber):
-        self.x = math.cos(2*math.pi/numOfPlayers*(snowManNumber+1)) * 200 + windowWidth/2
-        self.y = math.sin(2*math.pi/numOfPlayers*(snowManNumber+1)) * 200 + windowHeight/2
+class Snowman(pygame.sprite.Sprite):
+    def __init__(self, snowManNumber, img):
+        # Sprite stuff
+        pygame.sprite.Sprite.__init__(self)
+        self.trueImage = pygame.transform.scale(img, (50, 50))
+        self.image = pygame.transform.scale(img, (50, 50))
+        #self.image.fill(SnowManColors[snowManNumber])
+        self.rect = self.image.get_rect()
+
+        # Arranging the players in a circle
+        self.rect.center = (math.cos(2*math.pi/numOfPlayers*(snowManNumber+1)) * 200 + windowWidth/2,
+                            math.sin(2*math.pi/numOfPlayers*(snowManNumber+1)) * 200 + windowHeight/2)
         self.number = snowManNumber
         self.xVel = 0
         self.yVel = 0
+        self.score = 0
+        #self.color = SnowManColors[snowManNumber]
         
     def collide(self, otherx, othery):
         angle = math.atan2(self.y - othery, self.x - otherx)
@@ -28,25 +47,38 @@ class Snowman:
         self.yVel = math.sin(angle) * bounciness
         
 
-    def move(self):
-        if 20 < self.x + self.xVel < windowWidth - 20:
-            self.x += self.xVel
+    def update(self, others):
+        # Movement by velocities
+        if 20 < self.rect.centerx + self.xVel < windowWidth - 20:
+            self.rect.centerx += self.xVel
         else:
             self.xVel = 0
-
-        if 20 < self.y + self.yVel < windowHeight - 20:
-            self.y += self.yVel
+        if 20 < self.rect.centery + self.yVel < windowHeight - 20:
+            self.rect.centery += self.yVel
         else:
             self.yVel = 0
-            
-    def draw(self):
-        pygame.draw.circle(win, (255, 255, 255), (self.x, self.y), 20)
-        
 
-snowManList = []
+        self.image = pygame.transform.rotate(self.trueImage, math.atan2(self.yVel, self.xVel)/math.pi*180*2)
+        temp = self.rect
+        self.rect = self.image.get_rect()
+        self.rect.centerx = temp.centerx
+        self.rect.centery = temp.centery
+        pygame.draw.line(win, (0, 0, 0), 
+        (self.rect.centerx, self.rect.centery), 
+        (self.rect.centerx + math.cos(math.atan2(self.yVel, self.xVel))*40,
+        self.rect.centery + math.sin(math.atan2(self.yVel, self.xVel))*40), 2)
+        
+        for otherSnowman in others:
+            if otherSnowman.number != self.number:
+                if distance(self.rect.centerx, self.rect.centery, otherSnowman.rect.centerx, otherSnowman.rect.centery) <= 25:
+                    angle = math.atan2(self.rect.centery - otherSnowman.rect.centery, self.rect.centerx - otherSnowman.rect.centerx)
+                    self.xVel = math.cos(angle) * bounciness
+                    self.yVel = math.sin(angle) * bounciness
+
+snowManList = pygame.sprite.Group()
 
 for i in range(numOfPlayers):
-    snowManList.append(Snowman(i))
+    snowManList.add(Snowman(i, pygame.image.load("snowman.png").convert()))
 
 # Main game Loop
 running = True
@@ -62,20 +94,28 @@ while running:
 
     mousex, mousey = pygame.mouse.get_pos()
 
-    snowManList[0].x = mousex
-    snowManList[0].y = mousey
+    #snowManList[0].x = mousex
+    #snowManList[0].y = mousey
 
     win.fill((100, 100, 255))
 
-    for snowman in snowManList:
-        snowman.draw()
-        snowman.move()
-        for snowman2 in snowManList:
-            if distance(snowman.x, snowman.y, snowman2.x, snowman2.y) <= 40 and snowman.number != snowman2.number:
-                tempx = snowman.x
-                tempy = snowman.y
-                snowman.collide(snowman2.x, snowman2.y)
-                snowman2.collide(tempx, tempy)
+    for s in snowManList:
+        s.rect.centerx = pygame.mouse.get_pos()[0]
+        s.rect.centery = pygame.mouse.get_pos()[1]
+        break
+
+    snowManList.draw(win)
+    snowManList.update(snowManList)
+
+    #for snowman in snowManList: # Main player update loop
+    #    #snowman.draw() # Draw each player to the screen
+    #    snowman.update() # Move each player according to their velocities
+    #    for snowman2 in snowManList: # Looping through again for collision
+    #        # If collision
+    #        if distance(snowman.x, snowman.y, snowman2.x, snowman2.y) <= 40 and snowman.number != snowman2.number:
+    #            # Collide the two players
+    #            snowman.collide(snowman2.x, snowman2.y)
+    #            snowman2.collide(snowman.x, snowman.y)
 
     pygame.display.update()
 
