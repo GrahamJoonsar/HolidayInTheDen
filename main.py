88b95ad6e88594871d8ea0be721690e98f03cba4
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-09-08 21:46:07 -0400

    Added presents joles scores and new sprites

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
 import math
+import random
 
 # Window Setup
-windowWidth = 500
-windowHeight = 500
+windowWidth = 1366
+windowHeight = 768
+
+
 pygame.init()
 win = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption("SnowMan")
+
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
 numOfPlayers = 8
+numOfHoles = 10
+numOfPresents = 12
+
 bounciness = 3
+
 SnowManColors = [(255, 0, 0), 
                 (0, 255, 0),
                 (0, 0, 255), 
@@ -27,8 +38,8 @@ class Snowman(pygame.sprite.Sprite):
     def __init__(self, snowManNumber, img):
         # Sprite stuff
         pygame.sprite.Sprite.__init__(self)
-        self.trueImage = pygame.transform.scale(img, (50, 50))
-        self.image = pygame.transform.scale(img, (50, 50))
+        self.trueImage = pygame.transform.scale(img, (75, 75))
+        self.image = pygame.transform.scale(img, (75, 75))
         #self.trueImage.fill((100, 100, 255))
         self.rect = self.image.get_rect()
 
@@ -39,12 +50,7 @@ class Snowman(pygame.sprite.Sprite):
         self.xVel = 0
         self.yVel = 0
         self.score = 0
-        #self.color = SnowManColors[snowManNumber]
-        
-    def collide(self, otherx, othery):
-        angle = math.atan2(self.y - othery, self.x - otherx)
-        self.xVel = math.cos(angle) * bounciness
-        self.yVel = math.sin(angle) * bounciness
+        self.color = SnowManColors[snowManNumber]
         
 
     def update(self, others):
@@ -58,34 +64,121 @@ class Snowman(pygame.sprite.Sprite):
         else:
             self.yVel = 0
 
+        # Drawing the score above the player
+        text = myfont.render(str(self.score), False, self.color)
+        win.blit(text,(self.rect.centerx,self.rect.centery - 80))
+
         ## Rotating the sprite towards the direction its going
         # Getting the angle for rotation
         angle = math.atan2(self.yVel, self.xVel)/math.pi*180 - 90
         angle = -180 - angle
         
+        # Rotating the actual image
         self.image = pygame.transform.rotate(self.trueImage, angle)
         temp = self.rect
         self.rect = self.image.get_rect()
         self.rect.centerx = temp.centerx
         self.rect.centery = temp.centery
+
+
         # Debugging line showing direction
         #pygame.draw.line(win, (0, 0, 0), 
         #(self.rect.centerx, self.rect.centery), 
         #(self.rect.centerx + math.cos(math.atan2(self.yVel, self.xVel))*40,
         #self.rect.centery + math.sin(math.atan2(self.yVel, self.xVel))*40), 2)
         
+        # Collision
         for otherSnowman in others:
             if otherSnowman.number != self.number:
-                if distance(self.rect.centerx, self.rect.centery, otherSnowman.rect.centerx, otherSnowman.rect.centery) <= 25:
+                if distance(self.rect.centerx, self.rect.centery, otherSnowman.rect.centerx, otherSnowman.rect.centery) <= 50:
                     angle = math.atan2(self.rect.centery - otherSnowman.rect.centery, self.rect.centerx - otherSnowman.rect.centerx)
                     self.xVel = math.cos(angle) * bounciness
                     self.yVel = math.sin(angle) * bounciness
 
+class HoleInIce(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Sprite stuff
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(img, (75, 30))
+
+        self.rect = self.image.get_rect()
+        # Arranging the players in a circle
+        self.rect.center = (1, 1)
+
+    def changeLocation(self, otherHoles, otherPresents):
+        newX = random.randint(0, windowWidth - 76)
+        newY = random.randint(0, windowHeight - 51)
+
+        for hole in otherHoles:
+            if distance(hole.rect.centerx, hole.rect.centery, newX, newY) < 100:
+                self.changeLocation(otherHoles, otherPresents)
+                return
+
+        for present in otherPresents:
+            if distance(present.rect.centerx, present.rect.centery, newX, newY) < 100:
+                self.changeLocation(otherHoles, otherPresents)
+                return
+
+        self.rect.centerx = newX
+        self.rect.centery = newY
+
+
+class Present(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Sprite stuff
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(img, (75, 75))
+
+        self.rect = self.image.get_rect()
+        # Arranging the players in a circle
+        self.rect.center = (1, 1)
+
+    def changeLocation(self, otherHoles, otherPresents):
+        newX = random.randint(0, windowWidth - 76)
+        newY = random.randint(0, windowHeight - 51)
+
+        for hole in otherHoles:
+            if distance(hole.rect.centerx, hole.rect.centery, newX, newY) < 100:
+                self.changeLocation(otherHoles, otherPresents)
+                return
+
+        for present in otherPresents:
+            if distance(present.rect.centerx, present.rect.centery, newX, newY) < 100:
+                self.changeLocation(otherHoles, otherPresents)
+                return
+
+        self.rect.centerx = newX
+        self.rect.centery = newY
+
+    def update(self, pList, oholes, opresents):
+        for p in pList:
+            if distance(p.rect.centerx, p.rect.centery, self.rect.centerx, self.rect.centery) < 55:
+                p.score += 1
+                self.changeLocation(oholes, opresents)
+
+
+# All of the players
 snowManList = pygame.sprite.Group()
+holeList = pygame.sprite.Group()
+presentList = pygame.sprite.Group()
 
 # Initializing Players
 for i in range(numOfPlayers):
-    snowManList.add(Snowman(i, pygame.image.load("snowman.png").convert_alpha()))
+    snowManList.add(Snowman(i, pygame.image.load("penguin.png").convert_alpha()))
+
+# Initializing the holes
+for i in range(numOfHoles):
+    holeList.add(HoleInIce(pygame.image.load("holeInIce.png").convert_alpha()))
+
+for hole in holeList:
+    hole.changeLocation(holeList, presentList)
+
+# Initializing the presents
+for i in range(numOfPresents):
+    presentList.add(Present(pygame.image.load("present.png").convert_alpha()))
+
+for p in presentList:
+    p.changeLocation(holeList, presentList)
 
 # Main game Loop
 running = True
@@ -99,6 +192,7 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             
+    # Test movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         for s in snowManList:
@@ -116,14 +210,20 @@ while running:
         for s in snowManList:
             s.xVel += 0.1
             break
+    if keys[pygame.K_r]:
+        for p in presentList:
+            p.changeLocation(holeList, presentList)
+        for h in holeList:
+            h.changeLocation(holeList, presentList)
 
-    win.fill((100, 100, 255))
-
+    win.fill((140, 140, 255))
     
-
+    holeList.draw(win)
+    presentList.draw(win)
     snowManList.draw(win)
-    snowManList.update(snowManList)
 
+    snowManList.update(snowManList)
+    presentList.update(snowManList, holeList, presentList)
 
     pygame.display.update()
 
